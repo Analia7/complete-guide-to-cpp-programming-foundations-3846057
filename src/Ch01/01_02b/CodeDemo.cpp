1ebe420de7c04d00b61dc406704f0c7905bc111f
Analia Cabello Cano <54357187+Analia7@users.noreply.github.com>
2026-05-20 15:45:35 +0000

    1. Completed

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,17 @@
 // Complete Guide to C++ Programming Foundations
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+#include <iostream>
+
+/* 
+This is a comment over several lines
+
+*/
+
+int main(){
+    std::cout << "Hi There!" << std::endl;
+
+    std::cout << std::endl << std::endl;
+    return 0;
+}
